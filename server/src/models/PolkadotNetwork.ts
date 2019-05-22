72d0799430336790e76cab5ec9ab43c170365b42
zthomas <thomas@callings.com>
2019-05-22 16:15:42 -0700

    bugfix for missing async await

--- server/src/models/PolkadotNetwork.ts
+++ server/src/models/PolkadotNetwork.ts
@@ -8,17 +8,17 @@ async function getNetworkInfo(): Promise<Schema.NetworkInfo> {
 
 async function getNetworkNodes(): Promise<Polkadot.Node[]> {
   const nodes = await redis.get(redisKey.NETWORK_NODES);
-  return JSON.parse(nodes);
+  return nodes ? JSON.parse(nodes) : [];
 }
 
 async function getNetworkSnapshots(limit: number = config.SNAPSHOT_QUERY_LIMIT): Promise<Schema.NetworkSnapshot[]> {
-  const results = redis.lrange(redisKey.NETWORK_SNAPSHOTS, 0, limit);
-  return results.map(JSON.parse);
+  const results = await redis.lrange(redisKey.NETWORK_SNAPSHOTS, 0, limit);
+  return results ? results.map(JSON.parse) : [];
 }
 
 async function getLatestBlocks(): Promise<Schema.Block[]> {
   const results = await redis.lrange(redisKey.LATEST_BLOCKS, 0, config.BLOCK_HISTORY_LIMIT);
-  return results.map(JSON.parse);
+  return results ? results.map(JSON.parse) : [];
 }
 
 export default {
